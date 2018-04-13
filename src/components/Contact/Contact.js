affe2d8a9669521e9a86704cb3aad828ff72827f
Ting Wang <felix.wang.1026@gmail.com>
2018-04-13 19:26:53 -0400

    added contact form functions

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import './Contact.css';
 
 class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      text: ''
+    };
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
       <div className="contact">
@@ -11,16 +25,22 @@ class Contact extends Component {
           <input
             type="text"
             id="fname"
-            name="firstname"
+            name="firstName"
             placeholder="Your first name.."
+            onChange={this.handleChange}
+            value={this.state.firstName}
+            autoComplete="first-name"
           />
 
           <label htmlFor="lname">Last Name</label>
           <input
             type="text"
             id="lname"
-            name="lastname"
+            name="lastName"
             placeholder="Your last name.."
+            onChange={this.handleChange}
+            value={this.state.lastName}
+            autoComplete="last-name"
           />
 
           <label htmlFor="email">Email</label>
@@ -29,10 +49,20 @@ class Contact extends Component {
             id="email"
             name="email"
             placeholder="Your email.."
+            onChange={this.handleChange}
+            value={this.state.email}
+            autoComplete="email"
           />
 
           <label htmlFor="text">Text</label>
-          <textarea type="" id="text" name="text" placeholder="Your words.." />
+          <textarea
+            id="text"
+            name="text"
+            placeholder="Your words.."
+            onChange={this.handleChange}
+            value={this.state.text}
+            autoComplete="text"
+          />
 
           <input type="submit" value="Submit" />
         </form>
