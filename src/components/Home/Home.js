eb564d427a8b51c4b0dfe78d311ff654f932625d
Ting Wang <felix.wang.1026@gmail.com>
2019-04-10 21:56:05 -0400

    updated the resume link

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
         <div className="title">
           <h1>Hi, I'm Ting Wang</h1>
           <h2>Software Engineer</h2>
-          <a href="https://dl.airtable.com/ps0KJ1zwTX6q3AZT4FR4_ting_wang_resume.pdf" target="_blank" rel="noopener noreferrer"><h2>My Resume</h2></a>
+          <a href="https://dl.airtable.com/.attachments/9b83be7d1d69f189cede4a2d3e8e92ec/adc9d895/resume.pdf" target="_blank" rel="noopener noreferrer"><h2>My Resume</h2></a>
         </div>
       </div>
     );
