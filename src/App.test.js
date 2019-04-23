8b5902bb5359fde6664cfafca57be59eea8d4037
Ting Wang <felix.wang.1026@gmail.com>
2019-04-23 14:37:49 -0400

    modified the App.test.js file to add the router

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
