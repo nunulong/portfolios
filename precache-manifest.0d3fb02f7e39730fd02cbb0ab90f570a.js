caebdfcce33604fecad6027a894aab6fd32b70c3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-20 20:36:58 +0000

    Deploy nunulong/portfolios to github.com/nunulong/portfolios.git:gh-pages

---
+++ precache-manifest.0d3fb02f7e39730fd02cbb0ab90f570a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c86f397412aea96510cf79c42616a53",
+    "url": "/portfolios/index.html"
+  },
+  {
+    "revision": "e33eba2cb9c9ad41ab31",
+    "url": "/portfolios/static/css/main.aa7bce39.chunk.css"
+  },
+  {
+    "revision": "f18451e2d44e15124bfd",
+    "url": "/portfolios/static/js/2.f40b8516.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolios/static/js/2.f40b8516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e33eba2cb9c9ad41ab31",
+    "url": "/portfolios/static/js/main.ec7b83eb.chunk.js"
+  },
+  {
+    "revision": "20c564d615b98b774c6d",
+    "url": "/portfolios/static/js/runtime-main.135272b5.js"
+  },
+  {
+    "revision": "9c4992792e3c3979a3fffbae720af1fa",
+    "url": "/portfolios/static/media/background.9c499279.jpg"
+  },
+  {
+    "revision": "67fd6122ceb07667db80b8b76a307e92",
+    "url": "/portfolios/static/media/contact-background.67fd6122.jpg"
+  },
+  {
+    "revision": "3a2a63b62b9a56cc91b10e5b95630189",
+    "url": "/portfolios/static/media/github-battle.3a2a63b6.png"
+  },
+  {
+    "revision": "cadbe636288d75ee55007e9fc9be90a6",
+    "url": "/portfolios/static/media/metronome.cadbe636.png"
+  },
+  {
+    "revision": "a67b828ec857ef971b9b608a548d96fd",
+    "url": "/portfolios/static/media/random-quote.a67b828e.png"
+  },
+  {
+    "revision": "560bd5bcdc45326119e8ad22c8f164f4",
+    "url": "/portfolios/static/media/skills-background.560bd5bc.jpg"
+  },
+  {
+    "revision": "9cf3edd37edfa2c598e79fb692ad2f7f",
+    "url": "/portfolios/static/media/sweet.9cf3edd3.jpg"
+  },
+  {
+    "revision": "54d64409a7fb4b07026531edcdb287c6",
+    "url": "/portfolios/static/media/weather-report.54d64409.png"
+  }
+]);
