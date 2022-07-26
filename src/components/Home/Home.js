b1bdce39e43057b398f27a42efc33aebf3605f58
Ting Wang <taim@Tings-MacBook-Pro.local>
2022-07-26 01:34:53 -0400

    update resume url

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,7 @@ class Home extends Component {
         <div className="title">
           <h1>Hi, I'm Ting Wang</h1>
           <h2>Software Engineer</h2>
-          <a href="https://dl.airtable.com/.attachments/7ee301cb5ece9d1358003b1a16328d86/770c33a2/resume.pdf" target="_blank" rel="noopener noreferrer"><h2>My Resume</h2></a>
+          <a href="https://airtable.com/appfD3d0z7ltzV5jB/tblAD5jsb4SsKYqqL/viwSQyca3IumZJaYJ/recbAoWwakcSFM714/fldpMTHnyAeyHnrfC/attBadQNbTm71IgdO?blocks=hide" target="_blank" rel="noopener noreferrer"><h2>My Resume</h2></a>
         </div>
       </div>
     );
